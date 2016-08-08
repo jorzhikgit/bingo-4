3aadfa5393cd3170ccb593a3b309f57dfff3dc67
Ray Anthony Madrona <ray.anthony.madrona@gmail.com>
2016-08-08 15:41:05 +0800

    Add tall attribute.

--- public/src/js/directives/cardDirective.js
+++ public/src/js/directives/cardDirective.js
@@ -25,6 +25,7 @@ define([
 
                 link: function (scope, element, attrs, ctrl) {
                     if (attrs.showExtension !== undefined) scope.extension = 1;
+                    if (attrs.tall !== undefined) scope.tall = 1;
 
                     if (!scope.letters) {
                         scope.letters = {
