d4340fb1c80cb4240f7b66c940ec77b4299f5b88
Ray Anthony Madrona <ray.anthony.madrona@gmail.com>
2016-08-23 16:20:28 +0800

    Apply access level and remove Configurations.

--- app/views/layout/left.blade.php
+++ app/views/layout/left.blade.php
@@ -12,17 +12,6 @@
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
-        <!-- search form -->
-        <!-- <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search..."/>
-                <span class="input-group-btn">
-                    <button type='submit' name='seach' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
-                </span>
-            </div>
-        </form> -->
-        <!-- /.search form -->
-        <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
             <li>
                 <a ui-sref="app">
@@ -37,23 +26,14 @@
                     <i class="fa fa-angle-left pull-right"></i>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a ui-sref="app.play"><i class="fa fa-play"></i> Play</a></li>
-
-                    <li><a ui-sref="app.maker"><i class="fa fa-legal"></i> Card Maker</a></li>
+                    @if($user->access_level >= 1)
+                        <li><a ui-sref="app.play"><i class="fa fa-play"></i> Play</a></li>
+                    @endif
+                    @if($user->access_level > 2)
+                        <li><a ui-sref="app.maker"><i class="fa fa-legal"></i> Card Maker</a></li>
+                    @endif
                 </ul>
             </li>
-            @if($user->access_level > 1)
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-cogs"></i>
-                        <span>Configurations</span>
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a ui-sref="app.user"><i class="fa fa-group"></i> Users</a></li>
-                    </ul>
-                </li>
-            @endif
         </ul>
     </section>
     <!-- /.sidebar -->
