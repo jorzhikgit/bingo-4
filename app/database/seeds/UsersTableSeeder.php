36af5e73d4911f42def045941e26ef8ae469d689
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-07 09:56:36 +0800

    Fix users seeder

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -15,18 +15,18 @@ class UsersTableSeeder extends Seeder
         ]);
 
         User::create([
-            'username' => 'play',
-            'password' => 'play',
-            'display_name' => 'Play User',
+            'username' => 'trial',
+            'password' => 'trial',
+            'display_name' => 'Trial User',
             'status' => 1,
             'access_level' => 2,
             'change_password' => 0
         ]);
 
         User::create([
-            'username' => 'trial',
-            'password' => 'trial',
-            'display_name' => 'Trial User',
+            'username' => 'play',
+            'password' => 'play',
+            'display_name' => 'Play User',
             'status' => 1,
             'access_level' => 1,
             'change_password' => 0
