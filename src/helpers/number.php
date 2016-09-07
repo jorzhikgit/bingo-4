c43e494e0091ef7a912d2a234a6fce73a8572dcb
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-07 10:08:45 +0800

    Add column_numbers() helper

--- src/helpers/number.php
+++ src/helpers/number.php
@@ -21,6 +21,31 @@ function number_column($number)
     return 'o';
 }
 
+function column_numbers($column)
+{
+    $column = strtolower($column);
+
+    if ($column === 'b') {
+        return range(1, 15);
+    }
+
+    if ($column === 'i') {
+        return range(16, 30);
+    }
+
+    if ($column === 'n') {
+        return range(31, 45);
+    }
+
+    if ($column === 'g') {
+        return range(46, 60);
+    }
+
+    if ($column === 'o') {
+        return range(61, 75);
+    }
+}
+
 function format_number_column($column)
 {
     $column = is_numeric($column) ? number_column($column) : $column;
