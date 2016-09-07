a8583d2e3a9fb0d2bc243399713ab78b658fb2af
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-07 09:29:53 +0800

    Add production users in seeder

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -6,11 +6,30 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'username' => 'user',
-            'password' => 'user',
-            'display_name' => 'User',
+            'username' => 'admin',
+            'password' => 'admin',
+            'display_name' => 'Admin',
+            'status' => 1,
+            'access_level' => 3,
+            'change_password' => 0
+        ]);
+
+        User::create([
+            'username' => 'play',
+            'password' => 'play',
+            'display_name' => 'Play User',
             'status' => 1,
             'access_level' => 2,
+            'change_password' => 0
+        ]);
+
+
+        User::create([
+            'username' => 'trial',
+            'password' => 'trial',
+            'display_name' => 'Trial User',
+            'status' => 1,
+            'access_level' => 1,
             'change_password' => 0
         ]);
     }
