674eeef03dd94e5bd3e834e57bcaab4cf03a4bc5
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-16 11:59:47 +0800

    Add active() scope query

--- src/Models/Parish.php
+++ src/Models/Parish.php
@@ -5,4 +5,9 @@ namespace SedpMis\Bingo\Models;
 class Parish extends BaseModel
 {
 	protected $fillable = ['name', 'branch_no', 'card_ranges', 'card_ranges'];
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', 1);
+    }
 }
