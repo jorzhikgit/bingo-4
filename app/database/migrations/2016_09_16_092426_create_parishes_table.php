fe327ddb633530d84c4681f07791336216ec188e
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-16 09:29:03 +0800

    Add parishes table migration

---
+++ app/database/migrations/2016_09_16_092426_create_parishes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateParishesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('parishes', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->smallInteger('branch_no')->unsigned();
+			$table->string('card_ranges')->comment('Format: start_1-end_1,start_2-end_2,...');
+			$table->boolean('is_active')->default(0);
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('parishes');
+	}
+
+}
