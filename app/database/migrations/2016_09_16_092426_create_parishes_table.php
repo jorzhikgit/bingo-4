090e374acca46a0649d804c89d6c8b7f50e223e3
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-20 10:39:51 +0800

    Add category in parishes table

--- app/database/migrations/2016_09_16_092426_create_parishes_table.php
+++ app/database/migrations/2016_09_16_092426_create_parishes_table.php
@@ -17,6 +17,7 @@ class CreateParishesTable extends Migration {
 			$table->increments('id');
 			$table->string('name');
 			$table->smallInteger('branch_no')->unsigned();
+			$table->enum('category', ['big', 'small'])->comment('Big or small parish.');
 			$table->string('card_ranges')->comment('Format: start_1-end_1,start_2-end_2,...');
 			$table->boolean('is_active')->default(0);
 			$table->timestamps();
