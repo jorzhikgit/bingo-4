71e3bc359b711f1468e92aa677151cfd49bb1cc9
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-20 10:44:39 +0800

    Add date in parishes table

--- app/database/migrations/2016_09_16_092426_create_parishes_table.php
+++ app/database/migrations/2016_09_16_092426_create_parishes_table.php
@@ -18,6 +18,7 @@ class CreateParishesTable extends Migration {
 			$table->string('name');
 			$table->smallInteger('branch_no')->unsigned();
 			$table->enum('category', ['big', 'small'])->comment('Big or small parish.');
+			$table->date('date');
 			$table->string('card_ranges')->comment('Format: start_1-end_1,start_2-end_2,...');
 			$table->boolean('is_active')->default(0);
 			$table->timestamps();
