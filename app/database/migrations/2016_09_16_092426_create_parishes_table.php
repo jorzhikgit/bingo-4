610c17a512ef718d8037150913b7c4eaacd4a1c9
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-20 10:48:22 +0800

    Change branch_no to branch, add columns in parishes table

--- app/database/migrations/2016_09_16_092426_create_parishes_table.php
+++ app/database/migrations/2016_09_16_092426_create_parishes_table.php
@@ -16,9 +16,11 @@ class CreateParishesTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('name');
-			$table->smallInteger('branch_no')->unsigned();
+			$table->string('branch');
 			$table->enum('category', ['big', 'small'])->comment('Big or small parish.');
 			$table->date('date');
+			$table->unsignedSmallInteger('no_of_members');
+			$table->unsignedSmallInteger('additional_members');
 			$table->string('card_ranges')->comment('Format: start_1-end_1,start_2-end_2,...');
 			$table->boolean('is_active')->default(0);
 			$table->timestamps();
